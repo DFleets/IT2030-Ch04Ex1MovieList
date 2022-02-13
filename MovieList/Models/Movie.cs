84d7791f6a81b2f8bba8116a18b3cd1667333738
Ravend20 <Fleetsd@yahoo.com>
2022-02-13 00:38:52 -0500

    Add project files.

---
+++ MovieList/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
+{
+    
+    public class Movie
+
+    {
+       
+        public int MovieId { get; set; }
+
+        [Required (ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889,2999, ErrorMessage = "Year must be after 1889.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Year must be between 1 and 5.")]
+        public int? Rating { get; set; }   
+        
+       [Required(ErrorMessage = "Please enter genre")]
+       public string GenreId { get; set; }
+       public Genre Genre { get; set; }
+
+        public string Slug => Name?.Replace('', '-').ToLower() + '-' + Year ? ToString();
+
+    }
+    public class Genre
+    {
+        public string GenreId { get; set; }
+        public string Name { get; set; }
+    }
+
+
+}
